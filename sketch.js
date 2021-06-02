5e7b42f4482ac487f064c245580125fac3c511f0
Rithika25 <77888266+Rithika25@users.noreply.github.com>
2021-06-02 16:38:22 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,32 @@
+var canvas,bg;
+var together;
+var jerry, jerryImg1,jerryImg2;
+
+function preload() {
+    bg = loadImage("images/garden.png");
+    jerryImg1=loadAnimation("images/jerryOne.png");
+    jerryImg2= loadAnimation("images/jerryTwo.png","images/jerryThree.png");
+    jerryImg3=loadAnimation("images/jerryFour.png");
+
+}
+
+function setup(){
+    canvas = createCanvas(1000,800);
+
+
+    jerry = createSprite(200, 600);
+    jerry.addAnimation("jerryStanding", jerryImg1);
+    jerry.scale = 0.15;
+
+}
+
+function draw() {
+
+    background(bg);
+
+ 
+    drawSprites();
+}
+
+
+ 
